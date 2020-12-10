309c65e9418329dd7cbeca914a7f35136dbc893f
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2020-12-11 00:49:56 +0530

    Html updated

---
+++ app.js
@@ -0,0 +1,22 @@
+var buttonTranslate = document.querySelector("#btn-translate");
+var ipText = document.querySelector("#inputText");
+var outputField = document.querySelector("#output");
+
+var sourceURL = "https://api.funtranslations.com/translate/morse.json";
+
+function getTranslationURL(input) {
+    return sourceURL + "?text=" + input;
+}
+
+function clickHandler() {
+    var textIp = ipText.value;
+    fetch(getTranslationURL(textIp))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputField.innerText = translatedText; // output
+           })
+
+}
+
+buttonTranslate.addEventListener("click",clickHandler);
